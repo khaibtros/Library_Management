d664d9e4487826d6e5e266a0a27eec2fd3bf1343
ninhlph <laiphamhaininh@gmail.com>
2026-06-18 08:29:46 +0700

    borrowCard

---
+++ backend/models/BorrowCard.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const borrowedBookSchema = new mongoose.Schema({
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 1,
+  }
+}, { _id: false });
+
+const borrowCardSchema = new mongoose.Schema({
+  reader: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  borrowedBooks: [borrowedBookSchema],
+  processedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  borrowDate: {
+    type: Date,
+    default: Date.now,
+  },
+  dueDate: {
+    type: Date,
+  },
+  returnDate: {
+    type: Date,
+  },
+  status: {
+    type: String,
+    enum: ['borrowed', 'returned', 'overdue', 'cancelled'],
+    default: 'borrowed',
+  }
+}, { timestamps: true });
+
+const BorrowCard = mongoose.model('BorrowCard', borrowCardSchema, 'borrow_cards');
+module.exports = BorrowCard;
